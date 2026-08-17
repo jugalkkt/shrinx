fd707417c95cc72cee28f3d644156aecb5bbd91d
jugalkkt <jugalkakkat@gmail.com>
2026-08-17 11:37:33 +0530

    project skeleton with plaintext server response

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+
+app = FastAPI()
+
+
+@app.get("/", response_class=PlainTextResponse)
+def read_root():
+    return "Hello, world!"
+
+

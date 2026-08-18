906998cc6012bd45fe648be99a002605f43aa8a9
jugalkkt <jugalkakkat@gmail.com>
2026-08-18 17:27:42 +0530

    update testcases

--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -1,5 +1,8 @@
 from app.core.decode import decode
 from app.core.encode import encode
+import pytest
 
-def test_roundtrip():
-    assert decode(encode("3844")) == "3844"
+@pytest.mark.parametrize("n",[0, 1, 61, 62, 3844, 999999, 238328] + list(range(5000)))
+def test_roundtrip(n):
+    assert decode(encode(n)) == str(n)
+

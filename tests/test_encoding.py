fe134877535c22532109045d85ce4611c6b13fa9
jugalkkt <jugalkakkat@gmail.com>
2026-08-18 17:31:12 +0530

    add comment explaining testcases

--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -2,6 +2,7 @@ from app.core.decode import decode
 from app.core.encode import encode
 import pytest
 
+# edge cases along with a thousand values
 @pytest.mark.parametrize("n",[0, 1, 61, 62, 3844, 999999, 238328] + list(range(5000)))
 def test_roundtrip(n):
     assert decode(encode(n)) == str(n)

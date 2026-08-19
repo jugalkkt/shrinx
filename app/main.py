da3d77d4f5678ebbe0cf0811fdecdc6e017f9a7d
jugalkkt <jugalkakkat@gmail.com>
2026-08-19 15:55:10 +0530

    init_beanie fn

--- app/main.py
+++ app/main.py
@@ -1,22 +1,29 @@
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
 from contextlib import asynccontextmanager
+from app.core.config import settings
+from app.models import Link, User
+from pymongo import AsyncMongoClient
+from beanie import init_beanie
+
+
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # ---- this part runs once, at startup ----
 
     # Create Async PyMongo client
-    client = AsyncMongoClient("mongodb://user:pass@host:27017")
+    client = AsyncMongoClient(settings.MONGODB_URI)
     #Init beanie with the Product document class
-    await init_beanie(database=client.db_name, document_models=[Product])
+    await init_beanie(database=client["shrinx_db"], document_models=[Link, User]) 
 
     yield  # <-- the app runs here, handling requests, until shutdown
 
     # ---- this part runs once, at shutdown ----
     print("shutting down")
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/", response_class=PlainTextResponse)

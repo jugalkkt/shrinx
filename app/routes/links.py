0a5cc4eae577cfbbf063ef237a4e4fd59d776571
jugalkkt <jugalkakkat@gmail.com>
2026-08-27 15:10:32 +0530

    post link request finished with collision control

--- app/routes/links.py
+++ app/routes/links.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, status
 from app.models import Link, User, clientRequest
 from fastapi.responses import RedirectResponse, JSONResponse
 from app.core.shortcode import generate_code
-from datetime import datetime
+from datetime import date, datetime
+from pymongo.errors import DuplicateKeyError
 # other imports you'll need
 
 router = APIRouter()
@@ -18,7 +19,25 @@ async def follow(code: str):
 @router.post("/links")
 async def create_link(request: clientRequest):
     # your logic here
-    short_code = generate_code(7)
-    created_at = datetime.now()
-    link1 = Link(short_code=short_code, destination=request.destination, user=user)
-    return link1
+    # temporary placegolder user
+    user = await User.find_one(User.email=="akshay1@gmail.com")
+    max_retry = 5
+    for i in range(max_retry):
+        try:
+            short_code = generate_code(7)
+            created_at = datetime.now()
+            link1 = Link(short_code=short_code, destination=request.destination, user=user, created_at=created_at)
+            await link1.insert()
+            return JSONResponse(
+                status_code=201,
+                content={"message": "Link created", "short_code":short_code}
+            )
+        except DuplicateKeyError as e:
+            print(f'attempt {i} failed!')
+    return JSONResponse(
+        status_code=500,
+        content={"message": "Server error"}
+    )
+
+    
+    

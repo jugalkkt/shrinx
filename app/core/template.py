30faa42537f3bb98e1ead7295b5919e67df97e43
jugalkkt <jugalkakkat@gmail.com>
2026-08-28 19:09:47 +0530

    update pyproject.toml with packages from requirements.txt

--- app/core/template.py
+++ app/core/template.py
@@ -1,4 +1,5 @@
 from fastapi.templating import Jinja2Templates
+import pathlib
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory=pathlib.Path(__file__).resolve().parent.parent.parent / "templates")
 

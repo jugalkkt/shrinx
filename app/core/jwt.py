8997d6576eb30a03a74ae1d7a4f38e9b4b924619
jugalkkt <jugalkakkat@gmail.com>
2026-08-28 14:55:55 +0530

    add jwt.py

---
+++ app/core/jwt.py
@@ -0,0 +1,13 @@
+import jwt
+from datetime import datetime, timedelta, timezone
+from app.core.config import settings
+
+def create_token(user_id: str) -> str:
+    payload = {
+        "sub": user_id,
+        "exp": datetime.now(timezone.utc) + timedelta(days=7),
+    }
+    return jwt.encode(payload, settings.jwt_secret, algorithm="HS256")
+
+def decode_token(token: str) -> dict:
+    return jwt.decode(token, settings.jwt_secret, algorithms=["HS256"])
